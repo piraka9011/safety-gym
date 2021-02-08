45ab1ddb52e58c271bdfc9b4a6c2072ffc0d7fc6
Anas Abou Allaban <aabouallaban@pm.me>
2021-02-08 10:49:29 -0500

    0.2.0

--- safety_gym/__init__.py
+++ safety_gym/__init__.py
@@ -1,4 +1,5 @@
 from safety_gym import envs
+from safety_gym import config
 
-__version__ = "0.1.0"
-__all__ = ["__version__", "envs"]
+__version__ = "0.2.0"
+__all__ = ["__version__", "envs", "config"]
